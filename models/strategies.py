0a20334b6da7ec8428dcb6db866cb7bca47b0a8e
Daniel Contreras <contrerasdaniel142@gmail.com>
2023-10-15 00:11:32 -0500

    cambio logica

--- models/strategies.py
+++ models/strategies.py
@@ -184,7 +184,7 @@ class Tr3nd:
             TelegramApi.send_text(f"Tr3nd: [Estado para nueva orden {trend_signal}]")
         
         if trend_signal == TrendSignal.ready_fast:
-            if self.intermediate_trend.value != self.main_trend.value and self.intermediate_trend.value == self.fast_trend.value:
+            if self.intermediate_trend.value != self.main_trend.value:
                 trend_signal = TrendSignal.anticipating
                 TelegramApi.send_text(f"Tr3nd: [Estado para nueva orden {trend_signal}]")
             elif self.fast_trend.value == self.main_trend.value:
@@ -194,6 +194,9 @@ class Tr3nd:
         elif trend_signal == TrendSignal.ready_intermediate:
             if self.intermediate_trend.value == self.main_trend.value and self.intermediate_trend.value == self.fast_trend.value:
                 trend_signal = TrendSignal.buy
+                TelegramApi.send_text(f"Tr3nd: [Estado para nueva orden {trend_signal}]")
+            elif self.intermediate_trend.value != self.main_trend.value and self.intermediate_trend.value == self.fast_trend.value:
+                trend_signal = TrendSignal.anticipating
                 TelegramApi.send_text(f"Tr3nd: [Estado para nueva orden {trend_signal}]")
                             
         if trend_signal == TrendSignal.buy:
