78b4b5efe903356770ebf4dd6d7b36250d28b90f
Daniel Contreras <contrerasdaniel142@gmail.com>
2023-10-16 17:36:28 -0500

    correccion error

--- models/strategies.py
+++ models/strategies.py
@@ -358,7 +358,7 @@ class Tr3nd:
         
         # Obtiene las barras desde mt5
         while True:
-            minute_1_rates = MT5Api.get_rates_from_pos(symbol, TimeFrame.MINUTE_15, 1,  10080)
+            minute_1_rates = MT5Api.get_rates_from_pos(symbol, TimeFrame.MINUTE_1, 1,  10080)
             hour_1_rates = MT5Api.get_rates_from_pos(symbol, TimeFrame.HOUR_1, 1,  10080)
             if minute_1_rates is not None and hour_1_rates is not None:
                 break
